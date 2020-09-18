90dd4387734139f0c72ceb92330f620e2bf71f2b
Stewart Glover <stewart.glover191@gmail.com>
2020-09-18 22:11:42 +0100

    added project model and home page color

---
+++ db/migrate/20200918211038_create_projects.rb
@@ -0,0 +1,12 @@
+class CreateProjects < ActiveRecord::Migration[6.0]
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.text :description
+      t.string :image
+      t.string :technology
+
+      t.timestamps
+    end
+  end
+end
